546c77686d72a040add9b82cbf3f04fb536fbc08
Mark Karchevsky <karchevskijmark1@gmail.com>
2025-03-16 19:07:19 +0300

    rabota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports:[NavbarComponent,RouterOutlet],
+    templateUrl:'./app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+export class AppComponent {}
